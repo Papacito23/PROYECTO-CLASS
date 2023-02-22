e47415e7845bbad53c086bd69c55e454dc92a9a8
coyar <coyar@LAPTOP-AMBUIBB6>
2023-02-21 18:13:31 -0700

    Agregar archivos de proyecto.

---
+++ PROYECTO 2/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PROYECTO_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int[] arreglo = new int[3];
+        byte contador = 0;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int valor = int.Parse(textbox1.Text);
+            arreglo[contador] = valor;
+            contador++;
+
+        }
+
+        private void Lista_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < arreglo.Length; i++)
+                listaarreglo.Items.Add(arreglo[i]);
+        }
+    }
+}
